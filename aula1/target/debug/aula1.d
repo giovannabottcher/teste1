81457d9e24e2f4974b32be0a744027c4e2842f16
giovannabottcher <giovannabottcher18@gmail.com>
2025-02-21 00:48:39 +0000

    feat: commit inicial

---
+++ aula1/target/debug/aula1.d
@@ -0,0 +1,1 @@
+/workspaces/teste1/aula1/target/debug/aula1: /workspaces/teste1/aula1/src/main.rs
---
+++ aula1/target/debug/deps/aula1-5ac427dbc7840858.d
@@ -0,0 +1,5 @@
+/workspaces/teste1/aula1/target/debug/deps/aula1-5ac427dbc7840858: src/main.rs
+
+/workspaces/teste1/aula1/target/debug/deps/aula1-5ac427dbc7840858.d: src/main.rs
+
+src/main.rs:
